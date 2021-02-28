470038476586e59db99edc49f5ddc5f0731dd3c0
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-02-28 11:01:38 +0100

    kill some of the code that could panic from the lev mar solver

--- src/solvers/levmar/mod.rs
+++ src/solvers/levmar/mod.rs
@@ -8,17 +8,17 @@ mod builder;
 mod test;
 mod weights;
 
+use crate::linalg_helpers::DiagDMatrix;
+use crate::solvers::levmar::weights::Weights;
 pub use builder::LevMarProblemBuilder;
 pub use levenberg_marquardt::LevenbergMarquardt as LevMarSolver;
 use num_traits::Float;
 use std::ops::Mul;
-use crate::linalg_helpers::DiagDMatrix;
-use crate::solvers::levmar::weights::Weights;
 
 /// helper structure that stores the cached calculations,
 /// which are carried out by the LevMarProblem on setting the parameters
-#[derive(Debug,Clone)]
-struct CachedCalculations<ScalarType:Scalar+ComplexField> {
+#[derive(Debug, Clone)]
+struct CachedCalculations<ScalarType: Scalar + ComplexField> {
     /// The current residual of model function values belonging to the current parameters
     current_residuals: DVector<ScalarType>,
     /// Singular value decomposition of the current function value matrix
@@ -26,7 +26,6 @@ struct CachedCalculations<ScalarType:Scalar+ComplexField> {
     /// the linear coefficients `$\vec{c}$` providing the current best fit
     linear_coefficients: DVector<ScalarType>,
 }
-
 
 /// TODO add weight matrix
 /// TODO Document
@@ -55,7 +54,7 @@ where
     /// those are updated on set_params. If this is None, then it indicates some error that
     /// is propagated on to the levenberg-marquardt crate by also returning None results
     /// by residuals() and/or jacobian()
-    cached : Option<CachedCalculations<ScalarType>>,
+    cached: Option<CachedCalculations<ScalarType>>,
 }
 
 /// TODO document and document panics!
@@ -73,21 +72,36 @@ where
     fn set_params(&mut self, params: &Vector<ScalarType, Dynamic, Self::ParameterStorage>) {
         self.model_parameters = params.iter().cloned().collect();
         // matrix of weighted model function values
-        let mut Phi_W = &self.weights * self
+        let mut Phi_w = self
             .model
             .eval(&self.x, self.model_parameters.as_slice())
-            .expect("Error evaluating function value matrix");
+            .ok()
+            .map(|Phi| &self.weights * Phi);
 
-        let current_svd = Phi_W.clone().svd(true, true);
+        // calculate the svd
+        let svd_epsilon = self.svd_epsilon;
+        let current_svd = Phi_w.as_ref().map(|Phi_w| Phi_w.clone().svd(true, true));
         let linear_coefficients = current_svd
-            .solve(&self.y_w, self.svd_epsilon)
-            .expect("Error calculating solution with SVD.");
-        let current_residuals = &self.y_w - Phi_W * &linear_coefficients;
-        self.cached = Some(CachedCalculations{
-            current_residuals,
-            current_svd,
-            linear_coefficients,
-        })
+            .as_ref()
+            .and_then(|svd| svd.solve(&self.y_w, svd_epsilon).ok());
+
+        // calculate the residuals
+        let current_residuals = Phi_w
+            .zip(linear_coefficients.as_ref())
+            .map(|(Phi_w, coeff)| &self.y_w - Phi_w * coeff);
+
+        // if everything was successful, update the cached calculations, otherwise set the cache to none
+        if let (Some(current_residuals), Some(current_svd), Some(linear_coefficients)) =
+            (current_residuals, current_svd, linear_coefficients)
+        {
+            self.cached = Some(CachedCalculations {
+                current_residuals,
+                current_svd,
+                linear_coefficients,
+            })
+        } else {
+            self.cached = None;
+        }
     }
 
     fn params(&self) -> Vector<ScalarType, Dynamic, Self::ParameterStorage> {
@@ -95,17 +109,25 @@ where
     }
 
     fn residuals(&self) -> Option<Vector<ScalarType, Dynamic, Self::ResidualStorage>> {
-        self.cached.as_ref().map(|cached|cached.current_residuals.clone())
+        self.cached
+            .as_ref()
+            .map(|cached| cached.current_residuals.clone())
     }
 
     #[allow(non_snake_case)]
     fn jacobian(&self) -> Option<Matrix<ScalarType, Dynamic, Dynamic, Self::JacobianStorage>> {
         //todo: make this more efficient by parallelizing
-        let mut jacobian_matrix = unsafe {
-            DMatrix::<ScalarType>::new_uninitialized(self.y_w.len(), self.model.parameter_count())
-        };
 
-        if let Some(CachedCalculations{ current_residuals, current_svd, linear_coefficients } ) = self.cached.as_ref() {
+        if let Some(CachedCalculations {
+            current_residuals,
+            current_svd,
+            linear_coefficients,
+        }) = self.cached.as_ref()
+        {
+            let mut jacobian_matrix = unsafe {
+                DMatrix::<ScalarType>::new_uninitialized(self.y_w.len(), self.model.parameter_count())
+            };
+
             let U = current_svd.u.as_ref().expect("Did not calculate U of SVD. This should not happen and indicates a logic error in the library.");
             // U transposed
             let U_t = U.transpose();
@@ -115,11 +137,12 @@ where
 
             for (k, mut jacobian_col) in jacobian_matrix.column_iter_mut().enumerate() {
                 // weighted derivative matrix
-                let mut Dk = &self.weights * self
-                    .model
-                    .eval_deriv(&self.x, self.model_parameters.as_slice())
-                    .at(k)
-                    .expect("Error evaluating model derivatives.");
+                let mut Dk = &self.weights
+                    * self
+                        .model
+                        .eval_deriv(&self.x, self.model_parameters.as_slice())
+                        .at(k)
+                        .expect("Error evaluating model derivatives.");
 
                 let Dk_c = &Dk * linear_coefficients;
                 let minus_ak: DVector<ScalarType> = U * (&U_t * (&Dk_c)) - Dk_c;
