b7d78f41d3b2d1f905f0d3d78fc6ce8079572dcd
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-11-10 19:53:40 +0100

    fix compile error in tests

--- tests/integration_tests/levmar.rs
+++ tests/integration_tests/levmar.rs
@@ -89,10 +89,8 @@ impl LeastSquaresProblem<f64, Dynamic, U5> for DoubleExponentialDecayFittingWith
         // populate jacobian
         //let ncols = 5;
         let nrows = self.x.len();
-        let mut jacobian = unsafe {
-            Matrix::<f64, Dynamic, U5, Self::JacobianStorage>::new_uninitialized(nrows)
-                .assume_init()
-        };
+        let mut jacobian =
+            Matrix::<f64, Dynamic, U5, Self::JacobianStorage>::zeros(nrows);
 
         jacobian.set_column(
             0,
