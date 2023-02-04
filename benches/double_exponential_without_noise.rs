1829d9e707d751df26edb67557ca13c28e6528e7
geo-ant <54497890+geo-ant@users.noreply.github.com>
2023-02-04 17:35:40 +0100

    fix clippy

--- benches/double_exponential_without_noise.rs
+++ benches/double_exponential_without_noise.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use common::*;
 use levenberg_marquardt::LeastSquaresProblem;
 use nalgebra::DVector;
