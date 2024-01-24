ae19275a4da8ad4a9529f772b09132fa219d5e4c
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-01-24 08:59:30 +0100

    benches, results are troubling: handrolled slower

--- benches/multiple_right_hand_sides.rs
+++ benches/multiple_right_hand_sides.rs
@@ -2,20 +2,22 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use levenberg_marquardt::LeastSquaresProblem;
 use levenberg_marquardt::LevenbergMarquardt;
 use nalgebra::ComplexField;
-
+use nalgebra::DMatrix;
+use nalgebra::DVector;
 use nalgebra::DefaultAllocator;
-
 use nalgebra::Dyn;
 use nalgebra::OVector;
 use nalgebra::RawStorageMut;
-
 use nalgebra::Storage;
 use nalgebra::U1;
-
 use pprof::criterion::{Output, PProfProfiler};
+use rand::distributions::uniform::SampleRange;
+use rand::Rng;
+use rand::SeedableRng;
 use shared_test_code::models::DoubleExpModelWithConstantOffsetSepModel;
 use shared_test_code::models::DoubleExponentialDecayFittingWithOffsetLevmar;
 use shared_test_code::*;
+use std::ops::Range;
 use varpro::model::SeparableModel;
 use varpro::prelude::SeparableNonlinearModel;
 use varpro::solvers::levmar::LevMarProblem;
@@ -23,103 +25,53 @@ use varpro::solvers::levmar::LevMarProblemBuilder;
 use varpro::solvers::levmar::LevMarSolver;
 
 /// helper struct for the parameters of the double exponential
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Clone, PartialEq, Debug)]
 struct DoubleExponentialParameters {
     tau1: f64,
     tau2: f64,
-    c1: f64,
-    c2: f64,
-    c3: f64,
+    coeffs: DMatrix<f64>,
 }
 
-fn build_problem<Model>(
+fn build_problem_mrhs<Model>(
     true_parameters: DoubleExponentialParameters,
     mut model: Model,
 ) -> LevMarProblem<Model>
 where
     Model: SeparableNonlinearModel<ScalarType = f64>,
-    DefaultAllocator: nalgebra::allocator::Allocator<f64, Dyn>,
-    DefaultAllocator: nalgebra::allocator::Allocator<f64, Dyn, Dyn>,
-    <DefaultAllocator as nalgebra::allocator::Allocator<f64, Dyn>>::Buffer: Storage<f64, Dyn>,
-    <DefaultAllocator as nalgebra::allocator::Allocator<f64, Dyn>>::Buffer: RawStorageMut<f64, Dyn>,
-    DefaultAllocator:
-        nalgebra::allocator::Allocator<(<f64 as ComplexField>::RealField, usize), Dyn>,
 {
-    let DoubleExponentialParameters {
-        tau1,
-        tau2,
-        c1,
-        c2,
-        c3,
-    } = true_parameters;
-
+    let DoubleExponentialParameters { tau1, tau2, coeffs } = true_parameters;
     // save the initial guess so that we can reset the model to those
     let params = OVector::from_vec_generic(Dyn(model.parameter_count()), U1, vec![tau1, tau2]);
-
-    let base_function_count = model.base_function_count();
-    let y = evaluate_complete_model_at_params(
-        &mut model,
-        params,
-        &OVector::from_vec_generic(Dyn(base_function_count), U1, vec![c1, c2, c3]),
-    );
+    let y = evaluate_complete_model_at_params_mrhs(&mut model, params, &coeffs);
     LevMarProblemBuilder::new(model)
-        .observations(y)
+        .multiple_observations(y)
         .build()
         .expect("Building valid problem should not panic")
 }
 
-/// solve the double exponential fitting problem using a handrolled model
-fn run_minimization_for_handrolled_separable_model(
-    problem: LevMarProblem<DoubleExpModelWithConstantOffsetSepModel>,
-) -> [f64; 5] {
+fn run_minimization<Model>(problem: LevMarProblem<Model>) -> (DVector<f64>, DVector<f64>)
+where
+    Model: SeparableNonlinearModel<ScalarType = f64> + std::fmt::Debug,
+{
     let result = LevMarSolver::new()
         .fit(problem)
         .expect("fitting must exit successfully");
     let params = result.nonlinear_parameters();
     let coeff = result.linear_coefficients().unwrap();
-    [params[0], params[1], coeff[0], coeff[1], coeff[2]]
+    (params, coeff)
 }
 
-/// solve the double exponential fitting problem using a separable model from the builder
-/// I should be able to unify this with the handrolled model, but I can't figure out how to do it
-/// because I cannot find the correct generic bounds to do it
-fn run_minimization_for_builder_separable_model(
-    problem: LevMarProblem<SeparableModel<f64>>,
-) -> [f64; 5] {
-    let result = LevMarSolver::new()
-        .fit(problem)
-        .expect("fitting must exit successfully");
-    let params = result.nonlinear_parameters();
-    let coeff = result.linear_coefficients().unwrap();
-    [params[0], params[1], coeff[0], coeff[1], coeff[2]]
-}
-
-/// solve the problem by using nonlinear least squares with levenberg marquardt
-/// from the levenberg marquardt crate only
-fn run_minimization_for_levenberg_marquardt_crate_problem(
-    problem: DoubleExponentialDecayFittingWithOffsetLevmar,
-) -> [f64; 5] {
-    let (problem, report) = LevenbergMarquardt::new()
-        // if I don't set this, the solver will not converge
-        .with_stepbound(1.)
-        .minimize(problem);
-    assert!(
-        report.termination.was_successful(),
-        "Termination not successful"
-    );
-    let params = problem.params();
-    [params[0], params[1], params[2], params[3], params[4]]
+/// create a random matrix with coefficients in the given range
+fn create_random_dmatrix(
+    (rows, cols): (usize, usize),
+    seed: u64,
+    range: Range<f64>,
+) -> nalgebra::DMatrix<f64> {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
+    nalgebra::DMatrix::from_fn(rows, cols, move |_, _| rng.gen_range(range.clone()))
 }
 
 fn bench_double_exp_no_noise_mrhs(c: &mut Criterion) {
-    let true_parameters = DoubleExponentialParameters {
-        tau1: 1.,
-        tau2: 3.,
-        c1: 4.,
-        c2: 2.5,
-        c3: 1.,
-    };
-
     // see here on comparing functions
     // https://bheisler.github.io/criterion.rs/book/user_guide/comparing_functions.html
     let mut group = c.benchmark_group("Double Exponential Without Noise");
@@ -127,61 +79,40 @@ fn bench_double_exp_no_noise_mrhs(c: &mut Criterion) {
     let x = linspace(0., 12.5, 1024);
     // initial guess for tau
     let tau_guess = (2., 6.5);
-    // function values
-    let f = x.map(|x: f64| {
-        true_parameters.c1 * (-x / true_parameters.tau1).exp()
-            + true_parameters.c2 * (-x / true_parameters.tau2).exp()
-            + true_parameters.c3
-    });
 
-    group.bench_function("Using Levenberg Marquardt Crate", |bencher| {
+    let dataset_size = 1000;
+    let linear_coeffs = create_random_dmatrix((3, dataset_size), 0, 0.0..100.0);
+
+    let true_parameters = DoubleExponentialParameters {
+        tau1: 1.,
+        tau2: 3.,
+        coeffs: linear_coeffs,
+    };
+    group.bench_function("Using Model Builder (MRHS)", |bencher| {
         bencher.iter_batched(
             || {
-                DoubleExponentialDecayFittingWithOffsetLevmar::new(
-                    // we make it easier for the solver by giving it the
-                    // correct guesses for the coefficients from the start
-                    // which is not gonna happen for realistic problems
-                    &[
-                        tau_guess.0,
-                        tau_guess.1,
-                        true_parameters.c1,
-                        true_parameters.c2,
-                        true_parameters.c3,
-                    ],
-                    &x,
-                    &f,
-                )
-            },
-            run_minimization_for_levenberg_marquardt_crate_problem,
-            criterion::BatchSize::SmallInput,
-        )
-    });
-
-    group.bench_function("Using Model Builder", |bencher| {
-        bencher.iter_batched(
-            || {
-                build_problem(
-                    true_parameters,
+                build_problem_mrhs(
+                    true_parameters.clone(),
                     get_double_exponential_model_with_constant_offset(
                         x.clone(),
                         vec![tau_guess.0, tau_guess.1],
                     ),
                 )
             },
-            run_minimization_for_builder_separable_model,
+            run_minimization,
             criterion::BatchSize::SmallInput,
         )
     });
 
-    group.bench_function("Handcrafted Model", |bencher| {
+    group.bench_function("Handcrafted Model (MRHS)", |bencher| {
         bencher.iter_batched(
             || {
-                build_problem(
-                    true_parameters,
+                build_problem_mrhs(
+                    true_parameters.clone(),
                     DoubleExpModelWithConstantOffsetSepModel::new(x.clone(), tau_guess),
                 )
             },
-            run_minimization_for_handrolled_separable_model,
+            run_minimization,
             criterion::BatchSize::SmallInput,
         )
     });
