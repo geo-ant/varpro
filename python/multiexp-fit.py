c656ed94ea596d632592ae09f732c05c4f7e4029
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-05-15 23:16:11 +0200

    fiddle with parameters until fit works, add random noise for ci calculation

--- python/multiexp-fit.py
+++ python/multiexp-fit.py
@@ -8,19 +8,24 @@ def multiexp_decay(t,c1,c2,c3,tau1,tau2):
 
 # here are the true parameters of the model for 
 # generating synthetic data
-c1 = 3.4
-c2 = 9.8
+c1 = 2.2
+c2 = 6.8
 c3 = 1.6
 tau1 = 2.4
 tau2 = 6.0
 
-tdata = np.linspace(0,15,1000)
-ydata = multiexp_decay(tdata,c1,c2,c3,tau1,tau2)
+np.random.seed(0xdeadbeef)
+tdata = np.linspace(0,20,1000)
+ydata = multiexp_decay(tdata,c1,c2,c3,tau1,tau2)+np.random.normal(size=1000,scale=0.01);
 
 model = lm.Model(multiexp_decay)
 
 # guess initial parameters and fit model
-params = model.make_params(c1 = 1.0,c2 = 5, c3 = 0.3,tau1 = 1.,tau2 = 9.)
+params = model.make_params(c1 = 1.0,c2 = 5, c3 = 0.3,tau1 = 1.,tau2 = 7.)
 result = model.fit(ydata,params,t=tdata)
 
 print(result.fit_report())
+cb = result.eval_uncertainty(sigma=0.88,dscale = 0.0001);
+
+# print(cb)
+print(result.ci_report())
